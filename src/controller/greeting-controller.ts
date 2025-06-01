6b029123d145800e244b6469e6d3215721d64f46
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-06-01 10:32:40 +0700

    fix version

--- src/controller/greeting-controller.ts
+++ src/controller/greeting-controller.ts
@@ -2,22 +2,26 @@ import { GreetingService } from "../service/greeting-service";
 import { Greeting } from "../model/greeting";
 import { BaseResponse, Logger } from "../utils";
 import userService from "../service/user-service";
+import { User, UserList } from "../model/user";
 
 export class GreetingController {
   static async getGreeting(name: string) {
     const message = GreetingService.generateGreeting(name);
     Logger.info(`Greeting generated for ${name}: ${message}`);
-    const result = {
-      greeting: message,
-      for: name,
-    }
-    return BaseResponse('Greeting fetched successfully', 'success',  result );
+    const greeting: Greeting = { message };
+    return BaseResponse("Greeting fetched successfully", "success", greeting);
   }
 
   static async getUsersHandler() {
     try {
-      const users = await userService.getAllUsers();
+      const users: User[] = await userService.getAllUsers();
       Logger.info(`Get User Controller | users: ${JSON.stringify(users)}`);
+
+      if (users.length === 0) {
+        Logger.warn("Get User Controller | No users found");
+        return BaseResponse("No users found", "notFound", { users: [] });
+      }
+
       return BaseResponse('User fetched successfully', 'success', { users: users });
     } catch (error) {
       Logger.error(`Get User Controller | getUser | error: ${(error as Error).message}`);
